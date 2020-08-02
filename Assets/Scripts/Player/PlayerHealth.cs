0e23046fac57d86b4d709eda2270df012fa5ad1a
icode365 <52480678+icode365@users.noreply.github.com>
2020-08-02 13:54:55 +0530

    Implementing HealthBar

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{  
+    [SerializeField] HealthBar healthBar;
+    [SerializeField] private int maxHealth = 100;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))     //Damage Condition Instead of Space bar
+        {
+            TakeDamage(10);
+        }
+
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
