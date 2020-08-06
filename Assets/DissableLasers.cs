e996375aa5e359d6cfcf0dd151584a6b420703ad
icode365 <52480678+icode365@users.noreply.github.com>
2020-08-06 12:00:51 +0530

    Try #3

---
+++ Assets/DissableLasers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissableLasers : MonoBehaviour
+{
+    [SerializeField] GameObject parent;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Bullet")
+        {
+            parent.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Intro/CutScenes2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutScenes2 : MonoBehaviour
+{
+
+    [SerializeField] Image[] image;
+    int currentImage;
+    int nextImage;
+    [SerializeField] Animator animator;
+    [SerializeField]
+    private float timeBetweenImageChange;
+
+    void Start()
+    {
+        currentImage = 0;
+        nextImage = currentImage + 1;
+        StartCoroutine(StartTransition());
+    }
+
+    IEnumerator StartTransition()
+    {
+        while (nextImage < image.Length)
+        {
+            yield return new WaitForSeconds(timeBetweenImageChange);
+            ChangeImage();
+        }
+    }
+
+    public void ChangeImage()
+    {
+        image[currentImage].gameObject.SetActive(false);
+        image[nextImage].gameObject.SetActive(true);
+        currentImage++;
+        nextImage++;
+    }
+}
