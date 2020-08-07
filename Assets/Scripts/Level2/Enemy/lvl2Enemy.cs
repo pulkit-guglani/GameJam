41c4acb0026143227d528448f2a9ef6405b0e579
Aman <amandeadpool1@gmail.com>
2020-08-07 17:04:38 +0530

    enemy added

--- Assets/Scripts/Level2/Enemy/lvl2Enemy.cs
+++ Assets/Scripts/Level2/Enemy/lvl2Enemy.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 
 public class lvl2Enemy : MonoBehaviour
 {
-    
+
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         transform.position += Vector3.right*Time.deltaTime;
+        //anim.SetTrigger("Walk");
     }
     // Update is called once per frame
    
